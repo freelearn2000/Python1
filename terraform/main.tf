4c4c7a7b74eb78c5438f1a923ce7f40c1f0ab0c0
Nada Musthafa <93783722+NadaMusthafa@users.noreply.github.com>
2023-02-21 12:06:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,17 +114,17 @@ resource "azurerm_virtual_machine" "vm" {
 }
 
 resource "azurerm_service_plan" "plan01" {
-  name                = var.app_sevice_plan_name
-  location            = azurerm_resource_group.rg01.location
-  resource_group_name = azurerm_resource_group.rg01.name
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "example" {
-  name                = var.app_service_name
-  location            = azurerm_resource_group.rg01.location
-  resource_group_name = azurerm_resource_group.rg01.name
+  name                = "example-app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.plan01.id
   https_only            = true
   site_config { 
