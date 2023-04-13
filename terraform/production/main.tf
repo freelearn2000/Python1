055d00e00a4630316c74a3762c1b09e64cd913a6
unknown <lubnashams91@gmail.com>
2023-04-13 12:43:19 +0530

    workflow updated

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -1,148 +1,131 @@
-terraform {
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
+# Define the Azure provider
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "my-resource-group-prod"
+# Define the resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
   location = "eastus"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "my-vnet-prod"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# Define the virtual network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "my-subnet-prod"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+# Define the subnets
+resource "azurerm_subnet" "app" {
+  name                 = "app-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "my-nsg-prod"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_subnet" "database" {
+  name                 = "database-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-  security_rule {
-    name                       = "http"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+# Define the app service plan
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
-  security_rule {
-    name                       = "ssh"
-    priority                   = 200
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  sku {
+    tier = "Standard"
+    size = "S1"
   }
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "my-nic-prod"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# Define the app service
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
-  ip_configuration {
-    name                          = "my-nic-ip-prod"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = null
+  site_config {
+    always_on                 = true
+    linux_fx_version          = "DOCKER|node:14.17-alpine"
+    http2_enabled             = true
+    remote_debugging_enabled  = false
+    websockets_enabled        = true
+    use_32_bit_worker_process = false
   }
 
-  tags = {
-    environment = "dev"
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
+
+  connection_string {
+    name  = "db"
+    type  = "SQLServer"
+    value = azurerm_sql_database.example.primary_connection_string
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_sql_database.example,
+  ]
 }
 
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "my-vm-prod"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_B1s"
+# Define the managed SQL database
+resource "azurerm_sql_server" "example" {
+  name                = "example-sql-server"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  version             = "12.0"
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+  administrator_login          = "exampleadmin"
+  administrator_login_password = "SuperSecretPassword123!"
 
-  storage_os_disk {
-    name              = "my-vm-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
-
-  os_profile {
-    computer_name  = "my-vm"
-    admin_username = "adminuser"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags = {
-    environment = "dev"
-  }
+  auto_grow_enabled         = true
+  backup_retention_days     = 7
+  geo_redundant_backup      = "Disabled"
+  infrastructure_encryption = "Enabled"
 }
 
-resource "azurerm_service_plan" "plan01" {
-  name                = "example-appserviceplan-prod"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B1"
-}
+resource "azurerm_sql_database" "example" {
+  name                = "example-sql-database"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_sql_server.example.name
+  edition             = "Standard"
+  collation_name      = "SQL_Latin1_General_CP1_CI_AS"
 
-resource "azurerm_linux_web_app" "example" {
-  name                = "example-app-service-prod"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan01.id
-  https_only            = true
-  site_config { 
-    application_stack {
-      python_version = "3.9"
+  sku {
+    name     = "Standard"
+    tier     = "Standard"
+    capacity = 10
+  }
+
+  zone_redundant            = false
+  read_scale                = "Disabled"
+  long_term_retention       = "Disabled"
+  auto_pause_delay_in_minutes = 60
+
+  # Create a firewall rule to allow access from the app subnet
+  dynamic "firewall_rule" {
+    for_each = azurerm_subnet.app.address_prefixes
+
+    content {
+      name                = "Allow from ${firewall_rule.key}"
+      start_ip_address    = firewall_rule.value
+      end_ip_address      = firewall_rule.value
     }
-}
+  }
+
+  depends_on = [
+    azurerm_sql_server.example,
+  ]
 }
 
-resource "azurerm_app_service_virtual_network_swift_connection" "example" {
-  app_service_id = azurerm_linux_web_app.example.id
-  subnet_id      = azurerm_subnet.subnet.id
-}
-
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_linux_web_app.example.id
-  repo_url           = "https://github.com/freelearn2000/Python1.git"
-  branch             = "main"
-  use_manual_integration = true
-  use_mercurial      = false
-}
